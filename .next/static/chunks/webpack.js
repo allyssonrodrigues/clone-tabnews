c4842ae6fd402670a0209975e8df4d2e0fe76668
allyssonrodrigues <allysson.r@outlook.com>
2024-09-07 20:29:59 -0300

    remove .next

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "01d165b0808833cd"; }
+/******/ 		__webpack_require__.h = function() { return "2566d6d4275e752c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
