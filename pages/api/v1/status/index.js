86d193008d5ee5656245afc7273090476680e2d6
allyssonrodrigues <allysson.r@outlook.com>
2024-09-12 15:09:08 -0300

    add migration scripts

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -13,11 +13,11 @@ async function status(request, response) {
     databaseMaxConnectionsResult.rows[0].max_connections;
 
   const databaseName = process.env.POSTGRES_DB;
+
   const databaseOpenConnectionsResult = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName],
   });
-
   const databaseOpenConnectionsValue =
     databaseOpenConnectionsResult.rows[0].count;
 
