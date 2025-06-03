ed2ea9fd86b7ea22377fac85ff27e62a584fa44e
Super User <root@DESKTOP-NDMUO63.>
2025-06-03 20:33:20 -0300

    changing process.env.node_env

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,8 +7,8 @@ test("GET to /api/v1/status", async () => {
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.version).toEqual("16.9");
+  expect(responseBody.dependencies.database.max_connections).toEqual(901);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
 
